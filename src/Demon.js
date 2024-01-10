7a26e12384b5d003f4368a1fba17c8216c23081a
Petr DG <devil-ghost@yandex.ru>
2024-01-10 20:56:35 +0300

    Добавление файлов

---
+++ src/Demon.js
@@ -0,0 +1,8 @@
+import Chatacter from './Character.js';
+
+export default class Demon extends Chatacter {
+  constructor(distance) {
+    super();
+    this.distance = distance;
+  }
+}
---
+++ src/Magician.js
@@ -0,0 +1,8 @@
+import Chatacter from './Character.js';
+
+export default class Magician extends Chatacter {
+  constructor(distance) {
+    super();
+    this.distance = distance;
+  }
+}
